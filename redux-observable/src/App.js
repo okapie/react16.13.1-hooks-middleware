25c4fff05772b85fbdf3359411251035c255eaf4
okapie <tatigreuze@yahoo.co.jp>
2020-06-27 13:25:10 +0900

    Move files concerning redux-observable.

--- redux-observable/src/App.js
+++ redux-observable/src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
-import HomeComponent from "./comparison-middlewares/redux-observable/page/index.js";
+import HomeComponent from "./comparison-middlewares/page/index.js";
 
 function App() {
   return (
