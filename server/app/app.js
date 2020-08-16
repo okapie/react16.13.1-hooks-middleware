95fcca0bdddfdffff45233c39e628078422bfa18
okapie <tatigreuze@yahoo.co.jp>
2020-08-16 09:42:48 +0900

    Fix bug along the following error message:   Request header field credentials is not allowed by access-control-allow-headers in preflight response.

--- server/app/app.js
+++ server/app/app.js
@@ -23,7 +23,7 @@ app.use(bodyParser.json());
 
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Credentials");
   next();
 })
 
