9dc81f7d0023ee2d4faf9cd095911ff9664c7416
okapie <tatigreuze@yahoo.co.jp>
2020-09-06 15:18:15 +0900

    Remove unused files and modules.

--- hooks-without-redux/src/index.js
+++ hooks-without-redux/src/index.js
@@ -1,25 +1,10 @@
 import React  from "react";
 import ReactDOM from "react-dom";
-import { Provider } from "react-redux";
-import { createStore, applyMiddleware, compose, combineReducers } from "redux";
-import thunk from "redux-thunk";
-import client from "axios";
 
 import "./App.css";
 import App from "./pages/";
-import users from "./redux/modules/reducer/";
-
-const reducer = combineReducers({
-  users,
-});
-
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-const thunkWithClient = thunk.withExtraArgument(client);
-const store = createStore(reducer, composeEnhancers(applyMiddleware(thunkWithClient)));
 
 ReactDOM.render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
+  <App />,
   document.getElementById("root")
 );
